046a48794e6f19ba6fe8e029e36c2c57c4c592f6
jpivi <pivijulien@gmail.com>
2024-07-09 22:27:22 +0200

    add the first cone demo from vtk-js documentation

--- main.js
+++ main.js
@@ -11,13 +11,8 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <h1>Test VTK</h1>
+    <a href="vtk-js-demo.html">Run Cone Demo</a>
   </div>
 `
 
